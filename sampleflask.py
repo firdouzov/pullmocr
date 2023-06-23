2828f7dd76486be3b89c87094f9b9d6a8b08829d
firdouzov <100355834+firdouzov@users.noreply.github.com>
2023-06-23 15:34:50 +0400

    Update sampleflask.py

--- sampleflask.py
+++ sampleflask.py
@@ -7,7 +7,7 @@ from PIL import Image
  
 #*** Backend operation
  
-pytesseract.pytesseract.tesseract_cmd='/Pytesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd='Pytesseract-OCR/tesseract.exe'
 # WSGI Application
 # Defining upload folder path
 UPLOAD_FOLDER = os.path.join('staticFiles', 'uploads')
